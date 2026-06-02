d78493de2fcb77f611a7851110fc9779d012a96f
leappower <leappower@users.noreply.github.com>
2026-06-02 13:18:38 +0000

    deploy: ab6eeff353188ff71b5884a2468fe06fcea258c7

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // SW_VERSION 由 build.sh 在 production 构建时自动注入（毫秒时间戳）
 // dev 模式保留此默认值
-var SW_VERSION = "v1780405529671";
+var SW_VERSION = "v1780406300332";
 
 self.addEventListener("install", function() {
   self.skipWaiting();
